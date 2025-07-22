27f69b7c92b0ecb42319836efdb221d32f722755
Selim <selimx2001x@gmail.com>
2025-07-22 15:06:40 +0300

    Refactored MC STG environment URL to be part of the json data file

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -13,30 +13,28 @@ public class TestClass {
     SHAFT.GUI.WebDriver driver;
     SHAFT.TestData.JSON testData;
 
-    String targetUrl = "https://money-collector-fe.azurewebsites.net/";
-
-    private By emailField = By.id("email");
-    private By passField = By.id("password");
-    private By agencyCounterTitle = By.xpath("/html/body/app-root/app-layout/div/div/main/app-dashboard/div[1]/div[4]/div[1]/h2");
-    private By getAgencyCounterValue = By.xpath("/html/body/app-root/app-layout/div/div/main/app-dashboard/div[1]/div[4]/div[1]/p");
+    private final By emailField = By.id("email");
+    private final By passField = By.id("password");
+    private final By getAgencyCounterValue = By.xpath("/html/body/app-root/app-layout/div/div/main/app-dashboard/div[1]/div[4]/div[1]/p");
 
     @Test
     public void navigateToMonecyCollectorAndAssertLandingPageIsLogin(){
-        driver.browser().navigateToURL(targetUrl)
+        driver.browser().navigateToURL(testData.getTestData("STG_Url"))
                 .and().assertThat().title().contains(testData.getTestData("landingPageTitle"));
     }
 
     @Test
     public void loginWithValidCredentials(){
-        driver.browser().navigateToURL(targetUrl)
+        driver.browser().navigateToURL(testData.getTestData("STG_Url"))
                 .and().element().type(emailField, testData.getTestData("superAdminEmail") + Keys.ENTER)
                 .and().element().type(passField, testData.getTestData("superAdminPass") + Keys.ENTER)
                 .and().browser().assertThat().title().contains("Dashboard");
     }
 
     @Test
+
     public void numberOfAgenciesIsDisplayedCorrectlyInDashboard(){
-        driver.browser().navigateToURL(targetUrl)
+        driver.browser().navigateToURL(testData.getTestData("STG_Url"))
                 .and().element().type(emailField, testData.getTestData("superAdminEmail") + Keys.ENTER)
                 .and().element().type(passField, testData.getTestData("superAdminPass") + Keys.ENTER)
                 .and().assertThat(getAgencyCounterValue).text().contains(testData.getTestData("agencyCounterValue"));
